ae2d00d98352da9492a6bf9560d658e494cda148
Jakob Madsen <Fuzziwig@programmer.net>
2021-05-05 09:55:25 +0200

    Some changes to DBAccess to make it safer and streamlined

--- TAOTool/DBAccess.cs
+++ TAOTool/DBAccess.cs
@@ -2,6 +2,7 @@
 using Dapper;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace TAOTool
 {
@@ -10,9 +11,16 @@ namespace TAOTool
         //method for saving readings list to our database by using a stored procedure and passing parameters to it
         public void createReadings(List<Reading> readingList)
         {
-            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("tao1DB")))
+            try
             {
-                connection.Execute("dbo.WriteReading @RDate, @MWH, @M3, @UserID", readingList);
+                using (IDbConnection connection = new SqlConnection(Helper.CnnVal()))
+                {
+                    connection.Execute("dbo.WriteReading @RDate, @MWH, @M3, @UserID", readingList);
+                }
+            }
+            catch (SqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
             }
         }
 
@@ -20,8 +28,9 @@ namespace TAOTool
         public bool isConnected ()
         {
             try { 
-                using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("tao1DB")))
+                using (IDbConnection connection = new SqlConnection(Helper.CnnVal()))
                 {
+                    //we may have a connection but can we query as well
                     var result = connection.Query<int>($"SELECT 1");
                     return result.AsList().Contains(1);
                 }
@@ -35,7 +44,7 @@ namespace TAOTool
         {
             try
             {
-                using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("tao1DB")))
+                using (IDbConnection connection = new SqlConnection(Helper.CnnVal()))
                 {
                     var result = connection.Query<int>($"SELECT COUNT(*) FROM Readings");
                     return result.AsList()[0];
--- TAOTool/Helper.cs
+++ TAOTool/Helper.cs
@@ -5,7 +5,7 @@ namespace TAOTool
     public static class Helper
     {
         //helper class to setup connectionstring from app.config
-        public static string CnnVal(string name)
+        public static string CnnVal(string name= "tao1DB")
         {
             return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
